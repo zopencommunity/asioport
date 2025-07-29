1db9e9bf74837d54adf7be2ceab30440373fffbb
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2025-07-29 10:12:45 -0400

    Update cicd-stable.groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/Findasioport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/asioport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/Findasioport.git'), string(name: 'PORT_DESCRIPTION', value: 'Asio C++ Librrary[D[D[D[D[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/asioport.git'), string(name: 'PORT_DESCRIPTION', value: 'Asio C++ Library' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
